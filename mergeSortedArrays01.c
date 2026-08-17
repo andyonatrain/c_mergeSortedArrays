7270d0029c5a6588b2385933c1e2997ded272834
andyonatrain <283124961+andyonatrain@users.noreply.github.com>
2026-08-17 22:52:57 +1000

    Merge and sort two arrays
    Implemented merging of two sorted arrays and sorting the result.

--- mergeSortedArrays01.c
+++ mergeSortedArrays01.c
@@ -1,13 +1,33 @@
-// temporary filler
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-int myNumbers[] = {3, 6, 8, 1, 4, 6};
-int i = 0;
+int compare(const void *a, const void *b) {
+    int i = *(const int *)a;
+    int j = *(const int *)b;
 
-for (i = 5; i >= 0; i--) {
-  printf("%d\n", myNumbers[i]);
+    if (i < j) return -1;
+    if (i > j) return 1;
+    return 0;
+}
+
+int main(void){
+int myNumbersA[] = {1, 4, 6, 8};
+int myNumbersB[] = {3, 5, 5, 6, 9, 10};
+int mergedNumbers[10];
+int w, x, y = 0, z;
+
+for (w = 0; w < 4; w++) {
+  mergedNumbers[y++] = myNumbersA[w];
+}
+
+for (x = 0; x < 6; x++) {
+    mergedNumbers[y++] = myNumbersB[x];
+}
+
+qsort(mergedNumbers, 10, sizeof(int), compare);
+
+for (z = 0; z < 10; z++) {
+    printf("%d\n", mergedNumbers[z]);
 }
 
 return 0;
